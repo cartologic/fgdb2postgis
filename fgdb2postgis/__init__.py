307cf8724fb2a84159922a4c706fe4eb49cec429
gmioannou <gmioannou@gmail.com>
2020-05-12 10:29:18 +0300

    Remove imports

--- fgdb2postgis/__init__.py
+++ fgdb2postgis/__init__.py
@@ -1,8 +1,5 @@
 __author__ = 'George Ioannou'
-__version__ = (0, 3, 3, 'final', 0)
-
-from fgdb2postgis.filegdb import FileGDB
-from fgdb2postgis.postgis import PostGIS
+__version__ = (0, 3, 5, 'final', 0)
 
 def get_current_version():
     from .version import get_version
